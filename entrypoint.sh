7b58a055e673217f62151d2103b6462688d82d7d
Yuto Urakami <21374754+YutoUrakami@users.noreply.github.com>
2020-01-28 19:25:12 +0900

    wip: auto-mergeオプション追加を試している

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,7 @@ if [[ -n $(git diff --numstat) ]]; then
     prComment=${7:-$prTitle}
     token=${8:-$GITHUB_TOKEN}
     label=$9
+    autoMerge=${10:-0}
 
     git config user.email "${email}"
     git config user.name "${userName}"
@@ -29,11 +30,18 @@ if [[ -n $(git diff --numstat) ]]; then
     git push origin ${branchName}
     data='{"title":"'${prTitle}'","head":"'${branchName}'","base":"'${baseBranch}'","body":"'${prComment}'"}'
     response=$(curl -X POST -H "Authorization: token ${token}" -H "Content-Type:application/json" --data "$data" https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls)
+    prUrl=$(echo "${response}" | jq -r ".url")
     issueNumber=$(echo "${response}" | jq -r ".number")
+    head=$(echo "${response}" | jq -r ".head | .sha")
     echo "${issueNumber}"
+    echo "${head}"
     if [[ -n ${label} ]]; then
       data='{"labels":["'${label}'"]}'
       curl -X POST -H "Authorization: token ${token}" -H "Content-Type:application/json" --data "$data" https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${issueNumber}/labels
+    fi
+    if [[ ${autoMerge} = 1 ]]; then
+      data='{"commit_title":"'${prTitle}'","commit_message":"'${prComment}'","sha":"'${head}'"}'
+      curl -X PUT -H "Authorization: token ${token}" -H "Content-Type:application/json" --data "$data" ${prUrl}/merge
     fi
 else
     echo "noop"
